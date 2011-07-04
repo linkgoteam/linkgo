aa353ea8711ead4a74de4d6d20690944e4158a56
iamsunshow <iamsunshow@gmail.com>
2011-07-04 12:46:49 +0800

    linkgoengine.js

---
+++ js/linkgoengine.js
@@ -0,0 +1,9 @@
+(function(window){
+	var LinkGoEngine = (function(){
+		var LinkGoEngine=function(){
+			return LinkGoEngine().init();
+		};
+		return LinkGoEngine;
+	})();
+	window.LinkGoEngine=LinkGoEngine;
+})(window);
