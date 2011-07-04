ca0e6c2c9be0c22fb0fa7ad06848dc2ed2228268
iamsunshow <iamsunshow@gmail.com>
2011-07-04 13:17:45 +0800

    201107041317

--- js/linkgoengine.js
+++ js/linkgoengine.js
@@ -5,7 +5,19 @@
 		};
 		LinkGoEngine.prototype = {
 			init: function(initSettings){
+				var settings = initSettings ? initSettings : null,
+				row = settings && typeof settings.row == "number" ? settings.row : null,
+				column = settings && typeof settings.column == "number" ? settings.column : null,
+				category = settings && typeof setings.category == "number" ? settings.category : null;
 
+				if(row && column){
+					this.matrix = [];	
+					var i = row * column;
+					while(--i>=0){
+						this.matrix[i] = i;
+					}
+				}
+				this.category = category;
 			},
 		};
 		return LinkGoEngine;
